b41136a1f5ea593c837aa3d013d2058a92ada315
justin <pyjun02@gmail.com>
2023-09-08 15:46:57 +0900

    temp

---
+++ dist/index.js
@@ -0,0 +1,2 @@
+import{useState as n}from"react";import{jsx as e,jsxs as r}from"react/jsx-runtime";function i(){let[t,o]=n(0);return r("div",{children:["Count: ",t,e("button",{onClick:()=>o(t+1),children:"increase"})]})}export{i as Counter};
+//# sourceMappingURL=index.js.map
